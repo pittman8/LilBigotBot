29daacd9ac12c7252195e3ac3bdef3c4029bb3ce
kurtfriedrichhotmail <kurtfriedrich@hotmail.com>
2019-10-17 13:32:09 -0700

    components now in routes

---
+++ angular-app/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+//import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ResourcesComponent } from './resources/resources.component';
+import { SearchComponent } from './search/search.component';
+import { AnalyticsComponent } from './analytics/analytics.component';
+
+const myRoutes: Routes = [
+ { path: 'search', component: SearchComponent },
+ { path: 'analytics', component: AnalyticsComponent },
+ { path: 'resources', component: ResourcesComponent }
+];
+
+
+@NgModule({
+ imports: 
+  [ RouterModule.forRoot(myRoutes) ]
+ ,
+ exports: [
+   RouterModule
+ ],
+ declarations: []
+})
+export class AppRoutingModule { }
+
--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { ResourcesComponent } from './resources/resources.component';
 import { SearchComponent } from './search/search.component';
 import { AnalyticsComponent } from './analytics/analytics.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AnalyticsComponent } from './analytics/analytics.component';
     AnalyticsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
