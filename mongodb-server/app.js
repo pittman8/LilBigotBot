05abb675190a24364db70aea98c02fc527151143
kurtfriedrichhotmail <kurtfriedrich@hotmail.com>
2019-10-17 18:13:24 -0700

    changed to port 80 for Azure

--- mongodb-server/app.js
+++ mongodb-server/app.js
@@ -7,7 +7,7 @@ require("./config/db");
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
