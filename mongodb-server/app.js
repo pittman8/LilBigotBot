680e88ebd8cbe141731a93f8617407592d394824
ianfjohnson <ianfrederickjohnson@gmail.com>
2019-10-20 14:28:40 -0700

    CORS installed

--- mongodb-server/app.js
+++ mongodb-server/app.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const cors = require('cors');
 const HelloController = require("./controllers/HelloController");
+
+var corsOptions = {
+  origin: 'localhost/4200',
+  //origin: 'http://example.com',     /// note, this has to be the URL that your client app is calling from
+  optionsSuccessStatus: 200       // some legacy browsers (IE11, various SmartTVs) choke on 204 
+}
 
 // db instance connection
 //require("./db");
@@ -9,7 +16,7 @@ const HelloController = require("./controllers/HelloController");
 const port = process.env.PORT || 80;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+app.use(cors(corsOptions))
 
 // API ENDPOINTS
 app
