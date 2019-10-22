e81dfdc756974c6bfd5c916641d8bdc4ccea3ce4
kurtfriedrichhotmail <kurtfriedrich@hotmail.com>
2019-10-21 20:04:35 -0700

    merging ian branch

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ResourcesComponent } from './resources/resources.component';
@@ -16,7 +17,8 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-app/src/app/search/search.component.ts
+++ angular-app/src/app/search/search.component.ts
@@ -6,9 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
+  input_search = 'example_user';
+  result = 'Output holder';
 
+  startSearch() {
+    let newResult = 'Searched for ' + this.input_search ;
+
+    //contact server to search 
+
+    this.result = newResult;
+  }
   constructor() { }
-
   ngOnInit() {
   }
 
