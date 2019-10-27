aee01b70c46827a07e2a3ec68c6563142d9b3ee3
ianfjohnson <ianfrederickjohnson@gmail.com>
2019-10-27 11:29:08 -0700

    Created t94 branch

--- mongodb-server/app.js
+++ mongodb-server/app.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const taskController = require("./controllers/TaskController");
+//const taskController = require("./controllers/TaskController");
+const helloController = require("./controllers/HelloController");
+const cors = require('cors')  // using this module to solve CORS problem
+// note the extra line in package.json to download this code
+
+var corsOptions = {
+  origin: 'http://localhost:4200',   // this URL must match the URL that the Angular app will call from
+//origin: 'kurtangularappfall2019.azurewebsites.net',   // this URL must match the URL that the Angular app will call from
+optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
+}
 
 // db instance connection
 require("./config/db");
@@ -15,6 +24,10 @@ app.use(bodyParser.json());
 // API ENDPOINTS
 
 app
+  .route("/hello")
+  .get(helloController.returnHello);
+/*
+app
   .route("/tasks")
   .get(taskController.listAllTasks)
   .post(taskController.createNewTask);
@@ -24,7 +37,7 @@ app
   .get(taskController.readTask)
   .put(taskController.updateTask)
   .delete(taskController.deleteTask);
-
+*/
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
