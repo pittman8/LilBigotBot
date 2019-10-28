0f897bbd96a1e280517b26dfacb4559fe529b6a5
Andrew Klimentyev <andrew.klimentyev@gmail.com>
2019-10-27 21:06:06 -0700

    work on showing total calls and slurs

--- angular-app/src/app/analytics/analytics.component.ts
+++ angular-app/src/app/analytics/analytics.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnalyticsComponent implements OnInit {
 
+  totalSwears = 8;
+  totalCalls = 1;
   constructor() { }
 
   ngOnInit() {
