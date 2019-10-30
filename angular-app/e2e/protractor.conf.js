52174f588bb88fd72fe1244a6c472adf389cdb3d
kurtfriedrichhotmail <kurtfriedrich@hotmail.com>
2019-10-29 17:27:02 -0700

    trying again

--- angular-app/e2e/protractor.conf.js
+++ angular-app/e2e/protractor.conf.js
@@ -16,7 +16,7 @@ exports.config = {
     'browserName': 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 'https://isit422clientfall2019.azurewebsites.net',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
